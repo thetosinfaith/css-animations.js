45d19462906352e238357cc3370308955af30fbd
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2024-05-25 14:31:20 +0100

    css animation.js

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css"
+
+function App() {
+  return (
+    <main>
+      <div className="container">
+        <div className="box box1">
+          <button className="styleButton">Click to buy</button>
+        </div>
+        <div className="box box2">
+          <button className="styleButton">Click to buy</button>
+        </div>
+        <div className="box box3">
+          <button className="styleButton">Click to buy</button>
+        </div>
+        <div className="box box4">
+          <button className="styleButton">Click to buy</button>
+        </div>
+        <div className="box box5">
+          <button className="styleButton">Click to buy</button>
+        </div>
+        <div className="box box6">
+          <button className="styleButton">Click to buy</button>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default App
